2696145135b2ec3f70b2e2e9f1fcef89a9f4874b
umesh kumar k <umeshkumark@outlook.com>
2026-06-25 18:03:33 +0530

    modified details

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export const SITE_CONFIG = {
     // ----- THEME VARIABLE -----
     // FEEL FREE TO CHANGE THIS TO ONE OF THE OPTIONS
     // Options: 'default', 'strategic', 'innovator', 'executive'
-  baseTheme: 'default',
+  baseTheme: 'innovator',
 };
